7651720ff9f28f645e5f92080431c129a1126cbc
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-10-27 17:04:33 +0200

    Add files via upload

---
+++ Exercise.4_2.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a,b;
+	int i = 1;
+	 //'++i' отличается от 'i++' тем, что 'i++' сначала выведет 'i', и только потом прибавит; а '++i' сначала прибавит, и только потом выведет 'i'
+	a = ++i * i++;
+	cout << ++i * i++ << endl;
+	cout << a << endl;
+	//Часть 2
+	cout << "" << endl;
+	cout << "Задание 2" << endl;
+	float f, g;
+	cout << "Введите количество галлонов" << endl;
+	cin >> g;
+	f = g / 7.481;
+	cout << "Футов в " << g << " галлонах - " << f << endl;
+	//Часть 3
+	cout << "" << endl;
+	cout << "Задание 3" << endl;
+	cout << "" << endl;
+	int m, n;
+	n = 20;
+	m = 20;
+	cout << "10" << endl;
+	cout << n << endl;
+	cout << --m << endl;
+	cout << "" << endl;
+	return 0;
+}
