1c06136f697bc1faf38f335028e0573a9eea7466
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-11-04 04:13:27 +0500

    Add files via upload

---
+++ Разадресация и разыменование.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russia");
+	//Разадресация
+	int a = 322;
+	int* b = &a;
+	cout << b << endl;
+	cout << " " << endl;
+	//Разыменование
+	a = 1337;
+	b = &a;
+	cout << *b << endl;
+	cout << " " << endl;
+	return 0;
+}
