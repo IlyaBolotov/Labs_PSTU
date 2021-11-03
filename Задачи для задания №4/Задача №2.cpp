597085a63eae68f78414e95a8c480edc4b9913d7
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-11-04 04:56:26 +0500

    Add files via upload

---
+++ Задачи для задания №4/Задача №2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL,"Russian");
+	float i,k;
+	cout << "Введите число галонов" << endl;
+	cin >> k;
+	i = k / 7.481;
+	cout << "Футов в " << k << " галонах: " << i << endl;
+	return 0;
+}
