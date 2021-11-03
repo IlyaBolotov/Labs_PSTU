e996308c04376fbb450872b5c56dce13753f4ebb
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-11-04 04:27:02 +0500

    Update Задача №1.cpp

--- Задачи для задания №4/Задача №1.cpp
+++ Задачи для задания №4/Задача №1.cpp
@@ -1,10 +1,8 @@
 ﻿#include <iostream>
 
-using namespace std;
 int main()
 {
-	setlocale(LC_ALL, "Russian");
 	int i = 1;
-	cout << ++i * i++ << endl;
+	std::cout << ++i * i++ << std::endl;
 	return 0;
 }
