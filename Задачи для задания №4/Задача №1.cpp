6bfc3623df247e7f8b9a3d0993e856ffc6e81955
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-11-04 04:44:21 +0500

    Update Задача №1.cpp

--- Задачи для задания №4/Задача №1.cpp
+++ Задачи для задания №4/Задача №1.cpp
@@ -5,6 +5,6 @@ int main()
 	int i = 1;
 	std::cout << "Значение выражения '++i * i++' = ";
 	std::cout << ++i * i++ << std::endl;
-	std::cout << "Значение i = " << i;
+	std::cout << "Значение i = " << i << endl;
 	return 0;
 }
