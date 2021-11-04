1b9b1bf216ca7ea173944c6f23a318b389f7af66
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-11-04 16:22:42 +0500

    Update Задача №2.cpp

--- Задачи для задания №4/Задача №2.cpp
+++ Задачи для задания №4/Задача №2.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL,"Russian");
+	const int a = 7.481;
 	float i,k;
 	cout << "Введите число галонов" << endl;
 	cin >> k;
-	i = k / 7.481;
+	i = k / a;
 	cout << "Футов в " << k << " галонах: " << i << endl;
 	return 0;
 }
