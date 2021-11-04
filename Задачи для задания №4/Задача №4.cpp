cd2bb7b65c1366a602553a80fcdd299e52601857
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-11-04 06:02:09 +0500

    Add files via upload

---
+++ Задачи для задания №4/Задача №4.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float F, C;
+	cout << "Введите температуру в градусах по шкале Фаренгейта\n";
+	cin >> F;
+	C = (F - 32) * 5 / 9.0;
+	cout << "Температура по шкале цельсия: " << C << endl;
+	return 0;
+}
