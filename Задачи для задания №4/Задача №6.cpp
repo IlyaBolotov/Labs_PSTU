e4a4370ef851c9d386e5e2f397f8e85aa37b7851
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-11-04 07:21:53 +0500

    Add files via upload

---
+++ Задачи для задания №4/Задача №6.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c;
+	cout << "Введите 2 числа, которые необходимо перемножить" << endl;
+	cin >> a >> b;
+	c = a * b;
+	cout << "Произведение " << a << " и " << b << " = " << c << endl;
+	return 0;
+}
---
+++ Задачи для задания №4/Задача №7.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char a, b, c, d, e;
+	cout << "Введите 5 символов\n";
+	cin >> a >> b >> c >> d >> e;
+	cout << "5 введённых символов в обратном порядке:\n";
+	cout << e << d << c << b << a << endl;
+	return 0;
+}
