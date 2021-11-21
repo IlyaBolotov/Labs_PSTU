31bf4fc7310d0ed8a84a57c0096c469898a02c11
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-11-22 00:44:29 +0500

    Add files via upload

---
+++ Programme(22.11.2021).cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rusiian");
+	int N, i, j, k;
+	cout << "Введите длину катета" << endl;
+	cin >> N;
+	for (i = 1; i <= N; ++i)
+	{
+		if ((i != N) && (i != 1))
+		{
+			k = ((N) * 2) - 2 - ((i - 1) * 2);
+			for (j = 1; j <= k; j++)
+			{
+				cout << " ";
+			}
+			cout << "*";
+			k = (i - 1) * 2 - 1;
+			for (j = 1; j <= k; j++)
+			{
+				cout << " ";
+			}
+			cout << "*" << endl;
+		}
+		else if (i == N)
+		{
+			for (j = 1; j <= N; j++)
+			{
+				cout << "* ";
+			}
+			cout << endl;
+		}
+		else if (i == 1)
+		{
+			k = N * 2 - 2;
+			for (j = 1; j <= k; j++)
+			{
+				cout << " ";
+			}
+			cout << "*" << endl;
+		}
+	}
+	return 0;
+}
